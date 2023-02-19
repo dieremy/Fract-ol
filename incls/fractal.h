40c06613448d3ecd910002ce4e82a2d8369d0a3b
TheWillParadox <100817004+dieremy@users.noreply.github.com>
2023-02-19 10:47:20 +0100

    Add files via upload

---
+++ incls/fractal.h
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractal.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robegarc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 10:52:00 by robegarc          #+#    #+#             */
+/*   Updated: 2023/02/10 10:52:05 by robegarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTAL_H
+# define FRACTAL_H
+
+# define HW 800
+
+# include "minilibx_mms_20191025_beta/mlx.h"
+# include "hook.h"
+# include "colors.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+
+typedef struct s_fractal
+{
+	double	zx; //Z's real part
+	double	zy; //Z's imaginary part
+	double	cx; //C's real part
+	double	cy; //C's imaginary part
+	double	x_start; //x1 negative(sx)
+	double	x_end; //x2 positive(dx)
+	double	y_start; //y1 negative(down)
+	double	y_end; //y2 positive(up)
+	double	zoom;
+	double	tmp;
+	double	x_shift;
+	double	y_shift;
+	double	x; //1. axis for nested cicle
+	double	y; //2. axis for nested cicle
+	char	*name;
+	void	*mlx;
+	void	*win;
+	void	*img;
+	int		*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	int		i;
+	int		max_i;
+	int		color;
+	int		shade;
+}				t_fractal;
+
+void	put_pixel_image(int x, int y, t_fractal *d, int color);
+void	description(int ac, char **av);
+void	julia_plane(t_fractal *julia);
+void	mand_plane(t_fractal *mBrot);
+void	mandelbrot(t_fractal *mBrot);
+void	which_fract(t_fractal *d);
+void    dou_plane(t_fractal *dou);
+void	julia(t_fractal *julia);
+void    mlx_look(t_fractal *d);
+void	douady(t_fractal *dou);
+void    str_put(t_fractal *d);
+void	zoom(t_fractal *d);
+int		mouse_hook(int button, int x, int y, t_fractal *d);
+int		ft_key_press(int keycode, t_fractal *d);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_close(t_fractal *d);
+
+#endif
---
+++ incls/hook.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hook.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robegarc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 10:51:42 by robegarc          #+#    #+#             */
+/*   Updated: 2023/02/10 10:51:46 by robegarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HOOK_H
+# define HOOK_H
+
+# define KEY_SHIFT  264
+# define KEY_ESC    53
+# define KEY_X      7
+# define KEY_PLUS   69
+# define KEY_MINUS  78
+# define KEY_DOWN   125
+# define KEY_UP     126
+# define KEY_RIGHT  124
+# define KEY_LEFT   123
+# define MOUSEDOWN  4
+# define MOUSEUP    5
+# define ON_DESTROY 17
+# define SHIFT_SX   257
+# define SHIFT_DX   258
+# define KEY_M      46
+# define KEY_J      38
+# define KEY_D      2
+# define KEY_Z      6
+
+#endif
