0b2ad61924abb19e692fad2e50eb836790366ce3
fedora <robertoremyg@gmail.com>
2023-11-15 18:26:40 +0100

    upgraded Makefile to work on both mac and linux. still missing hook_mac.h

---
+++ include/fractal.h
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractal.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robegarc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 10:52:00 by robegarc          #+#    #+#             */
+/*   Updated: 2023/02/10 10:52:05 by robegarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTAL_H
+# define FRACTAL_H
+
+# define HW 1000
+
+# if __APPLE__
+
+#  include "minilibx/minilibx_mac/mlx.h"
+#  include "hook_mac.h"
+
+# elif __linux__
+
+#  include "minilibx/minilibx-linux/mlx.h"
+#  include "minilibx/minilibx-linux/mlx_int.h"
+#  include "hook_linux.h"
+
+# endif
+
+# include "colors.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+# include <pthread.h> 
+# include <string.h>
+
+# define NUM_THREADS 20
+
+typedef struct s_fractal
+{
+	void	*mlx;
+	void	*win;
+	void	*img;
+	double	zx;
+	double	zy;
+	double	cx;
+	double	cy;
+	double	x_start;
+	double	x_end;
+	double	y_start;
+	double	y_end;
+	double	zoom;
+	double	tmp;
+	double	x_shift;
+	double	y_shift;
+	double	scaling;
+	float	x;
+	float	y;
+	int		*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	int		i;
+	int		max_i;
+	int		color;
+	int		shade;
+	char	*name;
+}				t_fractal;
+
+void	put_pixel_image(int x, int y, t_fractal *d, int color);
+void	julia_plane(t_fractal *julia);
+void	*mandelbrot(void *mBrot);
+void	mand_plane(t_fractal *mBrot);
+void	dou_plane(t_fractal *dou);
+void	which_fract(t_fractal *d);
+void	*julia(void *julia);
+void	mlx_look(t_fractal *d);
+void	description(char **av);
+void	*douady(void *dou);
+void	str_put(t_fractal *d);
+void	zoom(t_fractal *d);
+int		mouse_hook(int button, int x, int y, t_fractal *d);
+int		ft_key_press(int keycode, t_fractal *d);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_close(t_fractal *d);
+
+#endif
---
+++ include/hook_linux.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hook.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robegarc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 10:51:42 by robegarc          #+#    #+#             */
+/*   Updated: 2023/02/10 10:51:46 by robegarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HOOK_H
+# define HOOK_H
+
+# define KEY_SHIFT_L	65506
+# define KEY_SHIFT_R	65505
+# define KEY_ESC		65307
+# define KEY_X			120
+# define KEY_PLUS		61
+# define KEY_MINUS		45
+# define KEY_DOWN		65364
+# define KEY_UP			65362
+# define KEY_RIGHT		65363
+# define KEY_LEFT		65361
+# define KEY_M			109
+# define KEY_J			106
+# define KEY_D			100
+# define MOUSEDOWN		4
+# define MOUSEUP		5
+# define MOUSELEFT		1
+
+#endif
